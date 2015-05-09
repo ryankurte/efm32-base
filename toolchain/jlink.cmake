d733474547528e5c6b121c53a53256058f76b0ec
Ryan Kurte <ryankurte@gmail.com>
2015-05-09 18:47:25 +1000

    Force SWD interface for debugger. Having debugger connection problems again...

--- toolchain/jlink.cmake
+++ toolchain/jlink.cmake
@@ -17,14 +17,14 @@ add_custom_target(debug-server
 	)
 
 add_custom_target(flash 
-	COMMAND JLinkExe -device ${DEVICE} -speed 4000 -CommanderScript ${CMAKE_CURRENT_BINARY_DIR}/flash.jlink 
+	COMMAND JLinkExe -device ${DEVICE} -speed 4000 -if SWD -CommanderScript ${CMAKE_CURRENT_BINARY_DIR}/flash.jlink 
 	DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${TARGET}.bin
 	)
 
 add_custom_target(erase 
-	COMMAND JLinkExe -device ${DEVICE} -speed 4000 -CommanderScript ${CMAKE_CURRENT_LIST_DIR}/erase.jlink 
+	COMMAND JLinkExe -device ${DEVICE} -speed 4000 -if SWD -CommanderScript ${CMAKE_CURRENT_LIST_DIR}/erase.jlink 
 	)
 
 add_custom_target(reset 
-	COMMAND JLinkExe -device ${DEVICE} -speed 4000 -CommanderScript ${CMAKE_CURRENT_LIST_DIR}/reset.jlink 
+	COMMAND JLinkExe -device ${DEVICE} -speed 4000 -if SWD -CommanderScript ${CMAKE_CURRENT_LIST_DIR}/reset.jlink 
 	)
