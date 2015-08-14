8d2bb0e4d658d8b22372171f44a736c7a40d3d03
Ryan Kurte <ryankurte@gmail.com>
2015-08-14 17:53:49 +1200

    Fixed jlink targets to auto build

--- toolchain/jlink.cmake
+++ toolchain/jlink.cmake
@@ -8,17 +8,17 @@ configure_file(${CMAKE_CURRENT_LIST_DIR}/flash.in ${CMAKE_CURRENT_BINARY_DIR}/fl
 #Add JLink commands
 add_custom_target(debug 
 	COMMAND ${DEBUGGER} -tui -command ${CMAKE_CURRENT_LIST_DIR}/remote.gdbconf ${CMAKE_CURRENT_BINARY_DIR}/${TARGET} 
-	DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${TARGET}
+	DEPENDS ${TARGET}
 	)
 
 add_custom_target(debug-server 
 	COMMAND JLinkGDBServer -device ${DEVICE} -speed 4000 -if SWD
-	DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${TARGET}.bin
+	DEPENDS ${TARGET}
 	)
 
 add_custom_target(flash 
 	COMMAND JLinkExe -device ${DEVICE} -speed 4000 -if SWD -CommanderScript ${CMAKE_CURRENT_BINARY_DIR}/flash.jlink 
-	DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${TARGET}.bin
+	DEPENDS ${TARGET}
 	)
 
 add_custom_target(erase 
