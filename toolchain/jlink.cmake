dce7cba8f2587ab215c4bf292d6dd7c34d132839
Ryan Kurte <ryankurte@gmail.com>
2016-07-18 23:17:20 +1200

    Added short commands

--- toolchain/jlink.cmake
+++ toolchain/jlink.cmake
@@ -18,15 +18,39 @@ add_custom_target(debug-server
 	DEPENDS ${TARGET}
 	)
 
+add_custom_target(d 
+	COMMAND ${DEBUGGER} -tui -command ${CMAKE_CURRENT_LIST_DIR}/remote.gdbconf ${CMAKE_CURRENT_BINARY_DIR}/${TARGET} 
+	DEPENDS ${TARGET}
+	)
+
+add_custom_target(ds
+	COMMAND JLinkGDBServer -device ${DEVICE} -speed 4000 -if SWD
+	DEPENDS ${TARGET}
+	)
+
 add_custom_target(flash 
 	COMMAND JLinkExe -device ${DEVICE} -speed 4000 -if SWD -CommanderScript ${CMAKE_CURRENT_BINARY_DIR}/flash.jlink 
 	DEPENDS ${TARGET}
 	)
 
+add_custom_target(f 
+	COMMAND JLinkExe -device ${DEVICE} -speed 4000 -if SWD -CommanderScript ${CMAKE_CURRENT_BINARY_DIR}/flash.jlink 
+	DEPENDS ${TARGET}
+	)
+
+
 add_custom_target(erase 
 	COMMAND JLinkExe -device ${DEVICE} -speed 4000 -if SWD -CommanderScript ${CMAKE_CURRENT_LIST_DIR}/erase.jlink 
 	)
 
+add_custom_target(e 
+	COMMAND JLinkExe -device ${DEVICE} -speed 4000 -if SWD -CommanderScript ${CMAKE_CURRENT_LIST_DIR}/erase.jlink 
+	)
+
 add_custom_target(reset 
+	COMMAND JLinkExe -device ${DEVICE} -speed 4000 -if SWD -CommanderScript ${CMAKE_CURRENT_LIST_DIR}/reset.jlink 
+	)
+
+add_custom_target(r 
 	COMMAND JLinkExe -device ${DEVICE} -speed 4000 -if SWD -CommanderScript ${CMAKE_CURRENT_LIST_DIR}/reset.jlink 
 	)
