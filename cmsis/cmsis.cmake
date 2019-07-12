71d3818a0bbcb18514ad9c21bf37cf3a4a91be42
afernandes <afernan4e@gmail.com>
2019-07-12 08:36:18 -0400

    Refactored libraries into cmake library projects
    - Use project name instead of manually set target
    - Update for cmake 3.14

--- cmsis/cmsis.cmake
+++
@@ -1,5 +0,0 @@
-#CMSIS cmake inclusion file
-#Adds CMSIS headers to the build
-
-#Add the header inclusion
-include_directories(${CMAKE_CURRENT_LIST_DIR}/Include)
--- toolchain/jlink.cmake
+++ toolchain/jlink.cmake
@@ -4,38 +4,38 @@
 # This file is covered under the MIT license available at: https://opensource.org/licenses/MIT
 
 # Configure flasher script for the project
-set(BINARY ${TARGET}.bin)
+set(BINARY ${PROJECT_NAME}.bin)
 configure_file(${CMAKE_CURRENT_LIST_DIR}/flash.in ${CMAKE_CURRENT_BINARY_DIR}/flash.jlink)
 
 #Add JLink commands
-add_custom_target(debug 
-	COMMAND ${DEBUGGER} -tui -command ${CMAKE_CURRENT_LIST_DIR}/remote.gdbconf ${CMAKE_CURRENT_BINARY_DIR}/${TARGET} 
-	DEPENDS ${TARGET}
+add_custom_target(debug
+        COMMAND ${DEBUGGER} -tui -command ${CMAKE_CURRENT_LIST_DIR}/remote.gdbconf ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}
+        DEPENDS ${PROJECT_NAME}
 	)
 
 add_custom_target(debug-server 
 	COMMAND JLinkGDBServer -device ${DEVICE} -speed 4000 -if SWD
-	DEPENDS ${TARGET}
+        DEPENDS ${PROJECT_NAME}
 	)
 
-add_custom_target(d 
-	COMMAND ${DEBUGGER} -tui -command ${CMAKE_CURRENT_LIST_DIR}/remote.gdbconf ${CMAKE_CURRENT_BINARY_DIR}/${TARGET} 
-	DEPENDS ${TARGET}
+add_custom_target(d
+        COMMAND ${DEBUGGER} -command ${CMAKE_CURRENT_LIST_DIR}/remote.gdbconf ${CMAKE_CURRENT_BINARY_DIR}/${PROJECT_NAME}
+        DEPENDS ${PROJECT_NAME}
 	)
 
 add_custom_target(ds
 	COMMAND JLinkGDBServer -device ${DEVICE} -speed 4000 -if SWD
-	DEPENDS ${TARGET}
+        DEPENDS ${PROJECT_NAME}
 	)
 
 add_custom_target(flash 
-	COMMAND JLinkExe -device ${DEVICE} -speed 4000 -if SWD -CommanderScript ${CMAKE_CURRENT_BINARY_DIR}/flash.jlink 
-	DEPENDS ${TARGET}
+	COMMAND JLinkExe -device ${DEVICE} -speed 4000 -if SWD -CommanderScript ${CMAKE_CURRENT_BINARY_DIR}/flash.jlink
+        DEPENDS ${PROJECT_NAME}
 	)
 
 add_custom_target(f 
-	COMMAND JLinkExe -device ${DEVICE} -speed 4000 -if SWD -CommanderScript ${CMAKE_CURRENT_BINARY_DIR}/flash.jlink 
-	DEPENDS ${TARGET}
+	COMMAND JLinkExe -device ${DEVICE} -speed 4000 -if SWD -CommanderScript ${CMAKE_CURRENT_BINARY_DIR}/flash.jlink
+        DEPENDS ${PROJECT_NAME}
 	)
 
 
